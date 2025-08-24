ac6407473430ce6e1c24721d83d3daf7f225bf68
Alisson Pereira <alissonpef@gmail.com>
2025-08-24 16:57:31 -0300

    feat: implement auth middleware to protect routes

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,7 +1,11 @@
+// src/routes/book.routes.js
 const { Router } = require("express");
 const bookController = require("../controllers/book.controller");
+const { ensureAuth } = require("../middlewares/auth.middleware");
 
 const router = Router();
+
+router.use(ensureAuth);
 
 router.post("/books", bookController.createBook);
 router.get("/books", bookController.getAllBooks);
